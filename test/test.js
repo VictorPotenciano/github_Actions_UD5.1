8dfeedd2961b4529689ec0a195e581b2d6d26daf
victor <victor@vboxvictor.debian12.local>
2025-02-18 14:14:51 +0100

    Cambios test.yml

--- test/test.js
+++ test/test.js
@@ -1,59 +1,45 @@
-const app = require('../index.js');
+const request = require('supertest');
+const { expect } = require('chai');
+const app = require('./index'); 
 
-const _ = require('underscore');
+describe('GET /', () => {
+  it('should return a 200 status code and a song with an animal and its sound', async () => {
+    const res = await request(app)
+      .get('/')
+      .expect(200);
 
-const port = process.env.PORT || 3000;
-const animals = {
-    "cat": "meow",
-    "dog": "bark",
-    "eel": "hiss",
-    "bear": "growl",
-    "frog": "croak",
-    "lion": "roar",
-    "bird": "tweet",
-    "cow": "muuuuu"
-};
-
-function getAnimal() {
-  return _.sample(Object.entries(animals));
-}
-
-
-
-app.get('/', async (req, res, next) => {
-  try {
-    const [animal_name, sound] = getAnimal();
-    res.status(200).send(`
-      George Orwell had a farm.<br />
-      E-I-E-I-O<br />
-      And on his farm he had a ${animal_name}.<br />
-      E-I-E-I-O<br />
-      With a ${sound}-${sound} here.<br />
-      And a ${sound}-${sound} there.<br />
-      Here a ${sound}, there a ${sound}.<br />
-      Everywhere a ${sound}-${sound}.<br />
-    `);
-  } catch (error) {
-    next(error);
-  }
+    expect(res.text).to.include('George Orwell had a farm.');
+    expect(res.text).to.include('E-I-E-I-O');
+    expect(res.text).to.match(/And on his farm he had a \w+\./);
+    expect(res.text).to.match(/With a \w+-\w+ here\./);
+    expect(res.text).to.match(/And a \w+-\w+ there\./);
+    expect(res.text).to.match(/Here a \w+, there a \w+\./);
+    expect(res.text).to.match(/Everywhere a \w+-\w+\./);
+  });
 });
 
-app.get('/api', async (req, res, next) => {
-  try {
-    res.status(200).json(animals);
-  } catch (error) {
-    next(error);
-  }
+describe('GET /api', () => {
+  it('should return a 200 status code and the animals JSON', async () => {
+    const res = await request(app)
+      .get('/api')
+      .expect(200);
+
+    expect(res.body).to.be.an('object');
+    expect(res.body).to.have.all.keys('cat', 'dog', 'eel', 'bear', 'frog', 'lion', 'bird', 'cow');
+  });
 });
 
-// Middleware para manejar errores
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
+describe('Error handling', () => {
+  it('should return a 500 status code and an error message', async () => {
+    // Simulamos un error forzado en la ruta '/'
+    app.get('/force-error', (req, res, next) => {
+      next(new Error('Forced error'));
+    });
 
-const server = app.listen(port, () => {
-  console.log(`Launching server on http://localhost:${port}`);
-});
+    const res = await request(app)
+      .get('/force-error')
+      .expect(500);
 
-module.exports = server; 
+    expect(res.text).to.equal('Something went wrong!');
+  });
+});
